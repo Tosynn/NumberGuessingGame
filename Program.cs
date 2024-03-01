b15428b2b4121bb2a40f49ca61b6d2b9e2f18c74
Tosin Adewale <tosinadewale01@gmail.com>
2024-03-01 09:10:34 +0000

    Resolved user input validation issue.

--- Program.cs
+++ Program.cs
@@ -9,28 +9,36 @@
 
         bool win = false; //Intializes win to false to mark beginning of game
 
+        bool exit = false; 
+
         do 
         {
             Console.Write("Enter a number:"); // Collects output from user
 
             string userInput = Console.ReadLine(); //Reads input 
 
-            int numberEntered = int.Parse(userInput); //Parse input as integer while also validating that user only enters integers. 
+            int numberEntered;
 
-            if (numberEntered > guessNumber) 
+            if (!int.TryParse(userInput, out numberEntered)) //validates user input must be integer
             {
-                Console.WriteLine("Too High...Guess Lower!");
+                Console.WriteLine($"{userInput} is not an integer.");
             }
-            else if (numberEntered < guessNumber)
+            else
             {
-                Console.WriteLine("Too Low...Guess Higher");
+                if (numberEntered > guessNumber)
+                {
+                    Console.WriteLine("Too High...Guess Lower!");
+                }
+                else if (numberEntered < guessNumber)
+                {
+                    Console.WriteLine("Too Low...Guess Higher");
+                }
+                else if (numberEntered == guessNumber) //If the user input is equal to the guess number, user wins.
+                {
+                    Console.WriteLine("YOU WIN!");
+                    win = true;
+                }
             }
-            else if (numberEntered == guessNumber) //If the user input is equal to the guess number, user wins.
-            { 
-                Console.WriteLine("YOU WIN!");
-                win = true;
-            }
-
         } while (win == false);
         Console.WriteLine("Thank you for playing this game");
         Console.Write("Press any key to exit the game.");
